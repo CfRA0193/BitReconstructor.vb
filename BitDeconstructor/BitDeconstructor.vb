21e1be322a038164ed5a95a5a146df5b3c8e197a
draf <artem.drobanov@gmail.com>
2016-06-28 12:44:20 +0300

    Small RND fix.

--- BitDeconstructor/BitDeconstructor.vb
+++ BitDeconstructor/BitDeconstructor.vb
@@ -30,23 +30,23 @@ Public Module BitDeconstructor
                 End If
                 outStreams(i) = New BufferedStream(New FileStream(outFileName, FileMode.CreateNew))
             Next
-            Dim mixBuffer = New Byte(N - 1) {}
+            Dim buffer = New Byte(N - 1) {}
             For i = 0 To inputStream.Length - 1
                 Dim s = inputStream.ReadByte()
-                For j = 0 To mixBuffer.Length - 1
-                    mixBuffer(j) = s
+                For j = 0 To buffer.Length - 1
+                    buffer(j) = s
                 Next
                 Dim errorIdxs = GetErrorPositions(N)
                 For Each errorIdx In errorIdxs
-                    mixBuffer(errorIdx) = RND(s) Mod Byte.MaxValue
+                    buffer(errorIdx) = RND(s) Mod Byte.MaxValue
                 Next
                 If _scr IsNot Nothing Then
-                    For j = 0 To mixBuffer.Length - 1
-                        outStreams(j).WriteByte(_scr.ProcessByte(mixBuffer(j)))
+                    For j = 0 To buffer.Length - 1
+                        outStreams(j).WriteByte(_scr.ProcessByte(buffer(j)))
                     Next
                 Else
-                    For j = 0 To mixBuffer.Length - 1
-                        outStreams(j).WriteByte(mixBuffer(j))
+                    For j = 0 To buffer.Length - 1
+                        outStreams(j).WriteByte(buffer(j))
                     Next
                 End If
             Next
@@ -76,6 +76,7 @@ Public Module BitDeconstructor
     Private Function RND(seed As Byte) As Integer
         Dim R = _rnd.Next()
         Dim T = Now.Ticks
-        Return (seed + R + T) Mod Integer.MaxValue
+        Dim UL As ULong = CLng(seed) + CLng(R) + CLng(T)
+        Return UL Mod Integer.MaxValue
     End Function
 End Module
